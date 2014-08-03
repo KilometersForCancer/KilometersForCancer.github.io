8c07d0327a115cba3e378b8b3641aaecba21ba9e
tyleriguchi <tguchi3@gmail.com>
2014-08-03 12:13:11 -0700

    remove jcarousel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,21 +18,4 @@
  */
 $(function() {
   'use strict';
-
-  $('.jcarousel').jcarousel({
-    wrap: 'circular',
-    transitions: Modernizr.csstransitions ? {
-      transforms:   Modernizr.csstransforms,
-      transforms3d: Modernizr.csstransforms3d,
-      easing:       'ease'
-    } : false
-  });
-
-  $('.jcarousel-control-prev').jcarouselControl({
-    target: '-=1'
-  });
-
-  $('.jcarousel-control-next').jcarouselControl({
-    target: '+=1'
-  });
 });
