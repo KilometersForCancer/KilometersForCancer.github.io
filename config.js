90630f81ccd77fd07e15d98dbb5f7c50e8ce17a6
tyleriguchi <tguchi3@gmail.com>
2014-08-21 15:59:15 -0700

    initial commit

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 
 var config = {
   // Api key information
+  key: "e51e9fa02d0ef4bde3930d6d20834a17-us7"
 }
 
 module.exports = config;
--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 'use strict';
 
 var express = require('express'),
-    app     = express();
+    app     = express(),
+    parser  = require('body-parser'),
+    mcAPI   = require('./node_modules/mailchimp-api/mailchimp'),
+    config  = require('./config.js');
 
 app.set('views', __dirname + '/app');
 
 app.use(express.static(__dirname + '/app'));
+app.use(parser.urlencoded({ extended: false }));
+app.use(parser.json());
+app.use(parser.json({ type: 'application/vnd.api+json' }));
+
+var mc = new mcAPI.Mailchimp(config.key);
 
 app.get('/', function(req, res) {
   res.send('index');
 });
+
+// app.post('/', function(req, res) {
+//   var name = req.body.name;
+//   var email = req.body.email;
+// })
 
 var port = process.env.PORT || 3000;
 
